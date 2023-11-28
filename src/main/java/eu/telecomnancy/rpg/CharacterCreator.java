33609537380b4568f0798af9caa7f403e4d52528
LucasEBSkora <skora.lucas@gmail.com>
2023-11-28 07:04:28 -0300

    Ajout patron Factory Method

--- src/main/java/eu/telecomnancy/rpg/CharacterCreator.java
+++ src/main/java/eu/telecomnancy/rpg/CharacterCreator.java
@@ -1,5 +1,5 @@
 package eu.telecomnancy.rpg;
 
 public interface CharacterCreator {
-    public GameCharacter createCharacter(String name, int level, boolean isArmed, boolean isArmored);
+    GameCharacter createCharacter(String name, int level, boolean isArmed, boolean isArmored);
 }
--- src/test/java/eu/telecomnancy/rpg/CharacterCreatorTest.java
+++ src/test/java/eu/telecomnancy/rpg/CharacterCreatorTest.java
@@ -1,6 +1,5 @@
 package eu.telecomnancy.rpg;
 
-import eu.telecomnancy.rpg.*;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
