1ce87c976c803f6f24d24c21a1c9857536534f28
LucasEBSkora <skora.lucas@gmail.com>
2023-11-28 06:55:59 -0300

    Ajout patron Singleton (fait dans le premier TP)

---
+++ src/main/java/eu/telecomnancy/rpg/GameConfiguration.java
@@ -0,0 +1,21 @@
+package eu.telecomnancy.rpg;
+
+public class GameConfiguration {
+    public int difficulty;
+    public int maxPartySize;
+
+    private static GameConfiguration singleInstance;
+
+    private GameConfiguration(int difficulty, int maxPartySize) {
+        this.difficulty = difficulty;
+        this.maxPartySize = maxPartySize;
+    }
+
+    static public GameConfiguration getInstance() {
+        if (GameConfiguration.singleInstance == null) {
+            GameConfiguration.singleInstance = new GameConfiguration(1, 5);
+        }
+        return GameConfiguration.singleInstance;
+    }
+
+}
---
+++ src/test/java/eu/telecomnancy/rpg/GameConfigurationTest.java
@@ -0,0 +1,19 @@
+package eu.telecomnancy.rpg;
+
+import org.junit.jupiter.api.Test;
+
+import static eu.telecomnancy.rpg.GameConfiguration.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameConfigurationTest {
+
+    @Test
+    void getInstanceCreatesObject() {
+        GameConfiguration instance = getInstance();
+        assertNotNull(instance);
+        assertEquals(5, instance.maxPartySize);
+        instance.maxPartySize = 10;
+        GameConfiguration otherInstance = getInstance();
+        assertEquals(10, instance.maxPartySize);
+    }
+}
